657414140d3cf2bc2eae02e5fb4c75cfa0227107
Alin Iordache <iordache.alin@yahoo.com>
2023-09-28 09:02:56 +0300

    Lazy loading

--- src/snippets.ts
+++ src/snippets.ts
@@ -162,91 +162,155 @@ class SnippetsUI implements SnippetEvents {
     }
 }
 
+class LazyWidgetCtx {
+    id: string;
+    widget: Element;
+
+    constructor(id: string, widget: Element) {
+        this.id = id;
+        this.widget = widget;
+    }
+}
+
 export class Snippets {
+    private static widgetIndex = 0;
+
+    private lazyWidget: Record<string, LazyWidgetCtx> = {};
+    private viewportObserver: IntersectionObserver = undefined;
+    private options: IntersectionObserverInit = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.05,
+    };
+
     private snippetsConfig: SnippetsConfig;
     private static snippetsUI: SnippetsUI = new SnippetsUI();
-    
+
     private readonly GITHUB_SNIPPETS_URL = "/assets/algovis/";
-    
+
+    private setupLazyLoading(widgetId: string, widget: Element): void {
+        if (!widgetId || !widget)
+            return;
+
+        let self = this;
+        this.lazyWidget[widgetId] = new LazyWidgetCtx(widgetId, widget);
+
+        if (!this.viewportObserver)
+            this.viewportObserver = new IntersectionObserver((entries: IntersectionObserverEntry[], _observer: IntersectionObserver) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        let widgetId = entry.target.getAttribute('av-code-editor-id');
+                        let widCtx = self.lazyWidget[widgetId];
+
+                        if (!widCtx)
+                            return;
+
+                        self.createWidget(widCtx.widget);
+
+                        // Remove observer and context as widget is created
+                        delete self.lazyWidget[widgetId];
+                        this.viewportObserver.unobserve(widCtx.widget);
+                    }
+                })
+            }, this.options);
+
+        this.viewportObserver.observe(widget);
+    }
+
     constructor() {
         Snippets.snippetsUI.initialize();
 
-        let index = 0;
-        for (let widget of document.querySelectorAll("[class*=algovis]")) {
-            let configId: string = undefined;
+        for (let widget of document.querySelectorAll("[class=algovis]")) {
+            let codeEditorId = "code-editor-" + (Snippets.widgetIndex++).toString();
+            widget.setAttribute('av-code-editor-id', codeEditorId);
 
-            if (widget.hasAttribute('av-exercise')) {
-                let jsonFileAndId = widget.getAttribute('av-exercise').split(':');
-                configId = "exercises/" + jsonFileAndId[0];
-            } else if (widget.hasAttribute('config-id')) {
-                configId = "snippets/" + widget.getAttribute('config-id');
-            }
-
-            let codeEditorId = "code-editor-" + (index++).toString();
-
-            let widgetLoader = (mustacheSnippets: any[] = [], snippets: Snippet[] = []) => {
-                widget.innerHTML = MustacheIt.render(appTemplate, {
-                    codeEditorId: codeEditorId,
-                    levels: mustacheSnippets,
-                    code: widget.innerHTML
-                });
-
-                // Move attributes from existing hooked element to the created child div                
-                let childWidget = (widget.firstChild as HTMLElement)
-
-                for (let attr of [...widget.attributes]) {
-                    childWidget.setAttribute(attr.name, attr.value);
-                    widget.removeAttribute(attr.name);
-                }
-
-                new Scene(childWidget as HTMLElement, snippets, Snippets.snippetsUI);
-
-                // Don't create the Splitter if we have explicit verticalView or we are in portrait
-                let splitWidget: any = undefined;
-                let splitSizes = {
-                    sizes: [55, 45],
-                };
-
-                if (!Snippets.snippetsUI.orientationWatcher.matches && !childWidget.classList.contains('verticalView'))
-                    splitWidget = Split([childWidget.children[0], childWidget.children[1]], splitSizes);
-
-                Snippets.snippetsUI.orientationWatcher.addEventListener("change", function (e) {
-                    if (e.matches) {
-                        if (splitWidget)
-                            splitWidget.destroy();
-                    } else {
-                        if (!childWidget.classList.contains('verticalView'))
-                            splitWidget = Split([childWidget.children[0], childWidget.children[1]], splitSizes);
-                    }
-                });
-            };
-
-            if (configId) {
-                fetch(this.GITHUB_SNIPPETS_URL + configId)
-                    .then((response) => {
-                        if (!response.ok)
-                            return;
-
-                        response.text().then((config: string) => {
-                            this.snippetsConfig = new SnippetsConfig();
-                            this.snippetsConfig.loadFromJson(JSON.parse(config));
-
-                            let snippetsForLang = this.snippetsConfig.getSnippetsForLang(Localize.getLangStr());
-
-                            let mustacheSnippets: any[] = []
-                            for (let level in snippetsForLang) {
-                                mustacheSnippets.push({ 'level': level, 'snippets': snippetsForLang[level] });
-                            }
-
-                            let snippets = this.snippetsConfig.getSnippetsForAllLevels(snippetsForLang);
-                            widgetLoader(mustacheSnippets, snippets);
-                        });
-                    });
+            if (widget.hasAttribute('av-lazy')) {
+                this.setupLazyLoading(codeEditorId, widget);
             } else {
-                widgetLoader();
+                this.createWidget(widget);
             }
         }
     }
+
+    private getConfigId(widget: Element): string {
+        let configId: string = undefined;
+
+        if (widget.hasAttribute('av-exercise')) {
+            let jsonFileAndId = widget.getAttribute('av-exercise').split(':');
+            configId = "exercises/" + jsonFileAndId[0];
+        } else if (widget.hasAttribute('config-id')) {
+            configId = "snippets/" + widget.getAttribute('config-id');
+        }
+
+        return configId;
+    }
+
+
+    private createWidget(widget: Element) {
+        let configId = this.getConfigId(widget);
+
+        if (configId) {
+            fetch(this.GITHUB_SNIPPETS_URL + configId)
+                .then((response) => {
+                    if (!response.ok)
+                        return;
+
+                    response.text().then((config: string) => {
+                        this.snippetsConfig = new SnippetsConfig();
+                        this.snippetsConfig.loadFromJson(JSON.parse(config));
+
+                        let snippetsForLang = this.snippetsConfig.getSnippetsForLang(Localize.getLangStr());
+
+                        let mustacheSnippets: any[] = []
+                        for (let level in snippetsForLang) {
+                            mustacheSnippets.push({ 'level': level, 'snippets': snippetsForLang[level] });
+                        }
+
+                        let snippets = this.snippetsConfig.getSnippetsForAllLevels(snippetsForLang);
+                        this.widgetLoader(widget, mustacheSnippets, snippets);
+                    });
+                });
+        } else {
+            this.widgetLoader(widget);
+        }
+    }
+
+    private widgetLoader(widget: Element, mustacheSnippets: any[] = [], snippets: Snippet[] = []) {
+        widget.innerHTML = MustacheIt.render(appTemplate, {
+            codeEditorId: widget.getAttribute("av-code-editor-id"),
+            levels: mustacheSnippets,
+            code: widget.innerHTML
+        });
+
+        // Move attributes from existing hooked element to the created child div                
+        let childWidget = (widget.firstChild as HTMLElement)
+
+        for (let attr of [...widget.attributes]) {
+            childWidget.setAttribute(attr.name, attr.value);
+            widget.removeAttribute(attr.name);
+        }
+
+        new Scene(childWidget as HTMLElement, snippets, Snippets.snippetsUI);
+
+        // Don't create the Splitter if we have explicit verticalView or we are in portrait
+        let splitWidget: any = undefined;
+        let splitSizes = {
+            sizes: [55, 45],
+        };
+
+        if (!Snippets.snippetsUI.orientationWatcher.matches && !childWidget.classList.contains('verticalView'))
+            splitWidget = Split([childWidget.children[0], childWidget.children[1]], splitSizes);
+
+        Snippets.snippetsUI.orientationWatcher.addEventListener("change", function (e) {
+            if (e.matches) {
+                if (splitWidget)
+                    splitWidget.destroy();
+            } else {
+                if (!childWidget.classList.contains('verticalView'))
+                    splitWidget = Split([childWidget.children[0], childWidget.children[1]], splitSizes);
+            }
+        });
+    };
 
     onFullScreenEvent(isFullScreen: boolean): void {
         isFullScreen ? Snippets.snippetsUI.onHideAllPopovers() : Snippets.snippetsUI.onShowAllPopovers();
